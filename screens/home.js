e97e64225e04f58fa954b9f06c0b5d47ee9bfe01
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-05-23 15:19:18 +0200

    up

--- screens/home.js
+++ screens/home.js
@@ -9,10 +9,9 @@ import { Ionicons,MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default class HomeScreen extends React.Component {
   render() {
-    const { isFocused } = this.props.navigation;
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home! {isFocused?"focus":"no focus"}</Text>
+        <Text>Home!</Text>
         <MaterialCommunityIcons name="qrcode-scan" size={25} color={"gray"} />
       </View>
     );
